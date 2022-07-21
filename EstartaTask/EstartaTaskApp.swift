2ea697733129504a82a77b3ddfd31c3d771e69bd
Alaa Abutaha <alaa.abutaha@7egend.cr>
2022-07-21 10:28:42 +0300

    Initial Commit

---
+++ EstartaTask/EstartaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstartaTaskApp.swift
+//  EstartaTask
+//
+//  Created by alaa ajoury on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct EstartaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
